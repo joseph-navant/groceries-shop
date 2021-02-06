7d034e39b1d9c7aa0dc15dcbacac1a521ba5d15a
Joseph <joseph.morantnavarro@gmail.com>
2021-02-06 22:03:12 +0100

    feat(product-list): show groceries list

--- src/app/core/services/http/groceries.service.ts
+++ src/app/core/services/http/groceries.service.ts
@@ -1,9 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { Grocery } from '../../models/grocery';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class GroceriesService {
+  constructor(private readonly httpClient: HttpClient) {}
 
-  constructor() { }
+  getGroceries(page: number, limit = 10) {
+    return this.httpClient.get<Grocery[]>(
+      `${environment.baseUrl}?_page=${page}&_limit=${limit}`
+    );
+  }
 }
--- src/app/pages/product-list/product-list.page.ts
+++ src/app/pages/product-list/product-list.page.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IonInfiniteScroll } from '@ionic/angular';
+import { Grocery } from 'src/app/core/models/grocery';
+import { GroceriesService } from 'src/app/core/services/http/groceries.service';
 
 @Component({
   selector: 'app-product-list',
@@ -6,10 +9,53 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-list.page.scss'],
 })
 export class ProductListPage implements OnInit {
+  @ViewChild(IonInfiniteScroll)
+  infiniteScroll: IonInfiniteScroll;
+  groceries: Grocery[];
+  private page = 1;
 
-  constructor() { }
+  constructor(private readonly groceriesService: GroceriesService) {}
 
   ngOnInit() {
+    this.initGroceries();
   }
 
+  loadNextProductsPage() {
+    this.loadGroceries(++this.page);
+  }
+
+  private initGroceries() {
+    this.loadGroceries(this.page);
+  }
+
+  private loadGroceries(page: number) {
+    this.groceriesService.getGroceries(page).subscribe(
+      (groceries: Grocery[]) => {
+        this.groceries = this.groceries
+          ? this.groceries.concat(groceries)
+          : groceries;
+
+        this.page = page;
+
+        // TODO: add items count to server response
+        this.groceries.length === 1000
+          ? this.disableInfiniteScroll()
+          : this.completeInfiniteScroll();
+      },
+      (err) => {
+        console.error('🚀 ~ ProductListPage ~ initGroceries ~ err', err);
+        // TODO: handle error
+      }
+    );
+  }
+
+  private completeInfiniteScroll() {
+    if (this.infiniteScroll) {
+      this.infiniteScroll.complete();
+    }
+  }
+
+  private disableInfiniteScroll() {
+    this.infiniteScroll.disabled = true;
+  }
 }
